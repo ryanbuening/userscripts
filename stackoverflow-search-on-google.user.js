f1d334c8b911769d6a23223921accefd7ed68def
Ryan Buening <ryanbuening@gmail.com>
2021-12-20 21:51:14 -0500

    Update stackoverflow-search-on-google.user.js

--- stackoverflow-search-on-google.user.js
+++ stackoverflow-search-on-google.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         StackOverflow search on Google
-// @version      1.1
+// @version      1.2
 // @description  Adds a button to search StackOverflow posts with Google
 // @author       Ryan Buening
 // @namespace    https://ryanbuening.com
@@ -17,7 +17,7 @@ const appendRight = false;
 const queryRegex = /q=[^&]+/g;
 const siteRegex = /\+site(?:%3A|\:).+\.[^&+]+/g;
 const urlFilter = "+site%3Astackoverflow.com";
-const icon = '<svg aria-hidden="true" class="svg-icon iconLogoGlyphXSm native js-svg" width="18" height="18" viewBox="0 0 18 18"><path d="M14 16v-5h2v7H2v-7h2v5h10z" fill="#BCBBBB"></path><path d="M12.09.72l-1.21.9 4.5 6.07 1.22-.9L12.09.71zM5 15h8v-2H5v2zm9.15-5.87L8.35 4.3l.96-1.16 5.8 4.83-.96 1.16zm-7.7-1.47l6.85 3.19.63-1.37-6.85-3.2-.63 1.38zm6.53 5L5.4 11.39l.38-1.67 7.42 1.48-.22 1.46z" fill="#F48024"></path></svg>';
+let icon = '<svg aria-hidden="true" class="svg-icon iconLogoGlyphXSm native js-svg" width="18" height="18" viewBox="0 0 18 18"><path d="M14 16v-5h2v7H2v-7h2v5h10z" fill="#BCBBBB"></path><path d="M12.09.72l-1.21.9 4.5 6.07 1.22-.9L12.09.71zM5 15h8v-2H5v2zm9.15-5.87L8.35 4.3l.96-1.16 5.8 4.83-.96 1.16zm-7.7-1.47l6.85 3.19.63-1.37-6.85-3.2-.63 1.38zm6.53 5L5.4 11.39l.38-1.67 7.42 1.48-.22 1.46z" fill="#F48024"></path></svg>';
 const isImageSearch = /[?&]tbm=isch/.test(location.search);
 
 if (typeof trustedTypes !== 'undefined') {
