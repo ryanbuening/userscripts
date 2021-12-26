8bf1a66cc648295a444bdffe674c9d7566688667
Ryan Buening <ryanbuening@gmail.com>
2021-12-26 08:31:01 -0500

    Create hacker-news-quote-styling.user.js

---
+++ hacker-news-quote-styling.user.js
@@ -0,0 +1,56 @@
+// ==UserScript==
+// @name         Hacker News blockquote styling
+// @version      1.1
+// @description  Adds styling to blockquotes in Hacker News comments
+// @author       Ryan Buening
+// @license      MIT
+// @namespace    https://github.com/ryanbuening/userscripts
+// @match        https://news.ycombinator.com/*
+// @run-at       document-end
+// ==/UserScript==
+
+const [head] = document.getElementsByTagName('head');
+style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `
+.comment-quote {
+	background: #46464620;
+	font-style: italic;
+	color: #464646;
+	border-left-width:: 3px;
+	border-left-color: #46464650;
+	border-left-style: solid;
+	padding: 2px;
+	padding-left: 5px;
+};
+`;
+head.appendChild(style);
+
+document.querySelectorAll('.commtext').forEach(c => {
+	let quoteDiv = null;
+	c.childNodes.forEach(node => {
+		const commentLine = node.textContent || node.innerText;
+		// match on both "> Lorem ipsum" AND ">Lorem ipsum"
+		if (quoteDiv || commentLine.match(/^>/)) {
+			if (commentLine.startsWith('>')) {
+				const quoteText = commentLine.substring(commentLine.indexOf('>') + 1);
+				if (node.textContent)
+					node.textContent = quoteText;
+				else
+					node.innerText = quoteText;
+			}
+
+			if (!quoteDiv) {
+				quoteDiv = document.createElement('div');
+				quoteDiv.classList.add('comment-quote');
+				node.parentNode.insertBefore(quoteDiv, node);
+			}
+
+			quoteDiv.appendChild(node);
+
+			if (!commentLine.match(/^>+\s*$/)) {
+				quoteDiv = null;
+			}
+		}
+	});
+});
