f63c1f97dc0acd2ab58438b52812f2fb54465f6d
Ryan Buening <ryanbuening@gmail.com>
2024-03-07 19:09:45 -0500

    Add files via upload

---
+++ drag-text-to-search.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         Drag text to search
+// @version      1.0
+// @description  Drag text to search
+// @author       Ryan Buening
+// @license      MIT
+// @namespace    https://github.com/ryanbuening/userscripts
+// @run-at       document-start
+// ==/UserScript==
+
+(function() {
+  const isTextArea = element => ['email', 'number', 'password', 'search', 'tel', 'text', 'url', 'textarea'].includes(element.type) && !element.disabled;
+
+  const handleDragOver = event => {
+    if (event.dataTransfer.types.includes('text/uri-list')) {
+      event.dataTransfer.dropEffect = 'link';
+      event.preventDefault();
+    } else if (event.dataTransfer.types.includes('text/plain') && !isTextArea(event.target)) {
+      event.dataTransfer.dropEffect = 'link';
+      event.preventDefault();
+    }
+  };
+
+  const handleDrop = event => {
+    if (event.dataTransfer.types.includes('text/uri-list')) {
+      const url = event.dataTransfer.getData('URL');
+      window.open(url, '_blank');
+      event.preventDefault();
+    } else if (event.dataTransfer.types.includes('text/plain') && !isTextArea(event.target)) {
+      const keyword = event.dataTransfer.getData('text/plain');
+      const url = 'https://www.google.com/search?q=%s'.replace(/%s/gi, encodeURIComponent(keyword));
+      window.open(url, '_blank');
+      event.preventDefault();
+    }
+  };
+
+  document.addEventListener('dragover', handleDragOver, false);
+  document.addEventListener('drop', handleDrop, false);
+})();
